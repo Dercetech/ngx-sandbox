61cbfd2fe5e4b1b56510c6ffdc2f2c5451af2722
dercetech <Abadon1986>
2018-02-21 17:17:02 +0100

    RxJS sandbox initialization with switchMap example

---
+++ src/app/pages/operators/switch-map/switch-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jem-switch-map',
+  templateUrl: './switch-map.component.html',
+  styleUrls: ['./switch-map.component.scss']
+})
+export class SwitchMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
