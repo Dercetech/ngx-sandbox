d3ac2cc17591430bbf17ed414cd68c551004a8c7
dercetech <Abadon1986>
2018-02-21 14:11:20 +0100

    Initial testbed ready

--- src/app/framework/layout/directives/padding/padding.directive.ts
+++ src/app/framework/layout/directives/padding/padding.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[jemPadding]'
 })
-export class PaddingDirective {
+export class PaddingDirective implements OnInit {
 
-  constructor() { }
+  constructor(private _el: ElementRef,
+              private _renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    this._renderer.setStyle(this._el.nativeElement, 'padding', '16px');
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,18 @@ import {NgModule} from '@angular/core';
 
 import {LayoutModule} from '../framework/layout/layout.module';
 
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {LandingComponent} from './landing/landing.component';
+import {CustomMaterialModule} from '../framework/material/material.module';
 
 @NgModule({
   declarations: [
     LandingComponent
   ],
   imports: [
-    LayoutModule
+    FlexLayoutModule,
+    LayoutModule,
+    CustomMaterialModule
   ],
   exports: [],
 })
